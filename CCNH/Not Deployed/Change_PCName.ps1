2b753ceea4a582ffea8aa9dc42454e426f53585f
CCNHPaul <phanlon@nh-cc.org>
2025-08-04 14:19:37 -0400

    Update Change_PCName.ps1

--- CCNH/Not Deployed/Change_PCName.ps1
+++ CCNH/Not Deployed/Change_PCName.ps1
@@ -1,3 +1,4 @@
 $pcName = Read-Host -Prompt "Enter the computer name to join to the domain"
-Rename-Computer -NewName "$pcName" -DomainCredential (New-Object System.Management.Automation.PSCredential ("cc-nh\administrator", ("ptfb2009!!" | ConvertTo-SecureString -AsPlainText -Force)))
+Rename-Computer -NewName "$pcName" -DomainCredential (New-Object System.Management.Automation.PSCredential ("cc-nh\administrator", ("*****" | ConvertTo-SecureString -AsPlainText -Force)))
+
 -Force -Restart
