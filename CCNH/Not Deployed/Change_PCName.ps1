89044b8e284a7c37f464ba37bb26be7886ec5c9b
CCNHPaul <phanlon@nh-cc.org>
2025-10-28 13:13:15 -0400

    Delete CCNH directory

--- CCNH/Not Deployed/Change_PCName.ps1
+++
@@ -1,4 +0,0 @@
-$pcName = Read-Host -Prompt "Enter the computer name to join to the domain"
-Rename-Computer -NewName "$pcName" -DomainCredential (New-Object System.Management.Automation.PSCredential ("cc-nh\administrator", ("*****" | ConvertTo-SecureString -AsPlainText -Force)))
-
--Force -Restart
